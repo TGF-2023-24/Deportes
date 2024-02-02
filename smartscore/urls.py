66d7ec57f298851ed633b9105c910bbc84f02326
Ganifo <morenocanduela@gmail.com>
2024-02-02 14:59:30 +0100

    añadido buscador por nombre

--- smartscore/urls.py
+++ smartscore/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.signup_user, name='signup'),
     path('logout/', views.logout_user, name='logout'),
     path('player/<int:custom_id>/', views.player_detail, name='player_detail'),
+    path('search_player/', views.search_player, name='search_player'),
 ]
--- smartscore/views.py
+++ smartscore/views.py
@@ -64,5 +64,13 @@ def signup_user(request):
         return render(request, 'signup.html', context)
    
 
+def search_player(request):
+    if request.method == 'POST':
+        searched = request.POST['q']
+        players = Player.objects.filter(Nombre__icontains=searched)
+        return render(request, 'search_player.html', {'searched': searched, 'players': players})
+    else:
+        return render(request, 'search_player.html', {})
+
    
 
