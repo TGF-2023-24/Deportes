7850b6f624ebcacc7dcbfb346b0ebf3ac8bf6cee
Ganifo <morenocanduela@gmail.com>
2024-02-02 11:59:25 +0100

    cambios login y sign up, crear páginas para cada jugador

--- smartscore/views.py
+++ smartscore/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .forms import CreateUserForm
+from django.contrib.auth.decorators import login_required #utilizar este decorador para proteger las rutas que requieren autenticación
+#se usa @login_required(login_url='login') en la vista que se quiere proteger
 
 # Create your views here.
 
@@ -16,18 +18,26 @@ def home(request):
 def about(request):
     return render(request, 'about.html', {})
 
+def player_detail(request, custom_id):
+    player = Player.objects.get(custom_id=custom_id)
+    return render(request, 'player_detail.html', {'player': player})
+
 def login_user(request):
-    if(request.method == 'POST'):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if(user is not None):
-            login(request, user)
-            messages.success(request, 'Login successfully!')
-            return redirect('home')
-        else:
-            messages.success(request, 'Login failed!')
-    return render(request, 'login.html', {})
+    if request.user.is_authenticated: #habrá que cambiar el redirect, esto debe ser cutre, pero por el momento nos vale
+        return redirect('home') 
+    else:
+        if(request.method == 'POST'):
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if(user is not None):
+                login(request, user)
+                messages.success(request, 'Login successfully!')
+                messages.info(request, 'Welcome ' + username)
+                return redirect('home')
+            else:
+                messages.success(request, 'Login failed!')
+        return render(request, 'login.html', {})
 
 def logout_user(request):
     logout(request)
@@ -35,19 +45,23 @@ def logout_user(request):
     return redirect('home')
 
 def signup_user(request):
-    form = CreateUserForm()
-    
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'User created successfully!')
-            return redirect('login') # Redirigir al usuario a la página de inicio de sesión, hay que cambiarlo
-        else:
-            messages.error(request, 'An error occurred during the registration process')
-    
-    context = {'form': form}
-    return render(request, 'signup.html', context)
+    if request.user.is_authenticated: #habrá q cambiarlo tmb
+        return redirect('home') 
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'User created successfully!')
+                messages.info(request, 'Welcome ' + user)
+                return redirect('login') # Redirigir al usuario a la página de inicio de sesión, hay que cambiarlo
+            else:
+                messages.error(request, 'An error occurred during the registration process')
+        
+        context = {'form': form}
+        return render(request, 'signup.html', context)
    
 
    
