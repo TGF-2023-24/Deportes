86ef65ba8b4dbcda3c8ad76c1ea3234f595f074a
Ganifo <morenocanduela@gmail.com>
2024-02-14 15:32:48 +0100

    radar fixed

--- smartscore/static/js_content/player_detail.js
+++ smartscore/static/js_content/player_detail.js
@@ -20,6 +20,33 @@ document.addEventListener('DOMContentLoaded', function() {
         '125px,250px': 'STC'
     };
 
+    // Function to create the initial radar chart
+    function createInitialRadarChart(playerStats) {
+        var ctx = document.getElementById('radarChart').getContext('2d');
+        radarChart = new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: ['Goal', 'Curr ability', 'Poss lost', 'Penalty saved'],
+                datasets: [{
+                    label: 'Player Stats',
+                    data: [playerStats.Goal, playerStats.CAbil, playerStats.Poss_lost_90, playerStats.Penalty_sav],
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scale: {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    createInitialRadarChart(initialPlayerStats);
+
     // Add event listener for dot click
     dots.forEach(function(dot) {
         dot.addEventListener('click', function() {
@@ -60,12 +87,31 @@ document.addEventListener('DOMContentLoaded', function() {
               <li>Poss lost: ${data[position].Poss_lost_90}</li>
               <li>Penalty saved: ${data[position].Penalty_sav}</li>
             `;
-            
+
+            addDatasetToRadarChart(data[position], position);
+
             
          })
          .catch(error => {
              console.error('Error fetching position stats:', error);
          });
+    }
+
+    // Function to add a new dataset to the radar chart
+    function addDatasetToRadarChart(statsData, position) {
+        //Clear existing datasets
+        radarChart.data.datasets.splice(1);
+        // Add a new dataset with the data from the fetched position
+        radarChart.data.datasets.push({
+            label: position + ' Stats',
+            data: [statsData.Goal, statsData.CAbil, statsData.Poss_lost_90, statsData.Penalty_sav],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)', // Change color if needed
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        });
+
+        // Update the chart
+        radarChart.update();
     }
 
     
