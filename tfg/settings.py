ad1b0ae10e4aa5e767f10f1732b348a0fcd3e4b3
Iván Gallego Cuervo <101187828+AnthonyMartial07@users.noreply.github.com>
2024-02-21 18:02:01 +0100

    settings

--- tfg/settings.py
+++ tfg/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions' , # Add this line to install django-extensions and execute the import script
     'smartscore.apps.SmartscoreConfig', # Add this line to connect the app to the project
+    'corsheaders', # Add this line to install django-cors-headers
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # Add this line to enable CORS
 ]
 
 ROOT_URLCONF = 'tfg.urls'
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
